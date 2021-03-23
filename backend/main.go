6a48f3a75cb7aeb150550fd18457a9e2e23e0511
huantingwei <ting02940@gmail.com>
2021-03-23 21:28:49 +0800

    fix: add CORS

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,6 @@ import (
 
 	//internal package
 	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/cors"
 	"github.com/huantingwei/fyp/kubebench"
 	"github.com/huantingwei/fyp/kubescore"
 	"github.com/huantingwei/fyp/login"
@@ -25,7 +24,7 @@ func main() {
 	*/
 	router := gin.Default()
 
-	router.Use(cors.Default())
+	router.Use(CORSMiddleware())
 
 	v1 := router.Group("/api/v1")
 	{
@@ -40,4 +39,19 @@ func main() {
 	}
 
 	router.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
 }
