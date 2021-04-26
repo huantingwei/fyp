676bde1c44e7a81ebe9a0289492c60114ba8b55e
huantingwei <ting02940@gmail.com>
2021-04-26 12:30:59 +0800

    fix: enable kubescore optional test

--- backend/kubescore/interactive.sh
+++ backend/kubescore/interactive.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo docker run -v $(pwd):/project zegl/kube-score:v1.10.0 score "./backend/kubescore/interactive-in.yaml" -o json > "./backend/kubescore/interactive-out.json"
+format="json"
+
+sudo docker run -v $(pwd):/project zegl/kube-score:v1.10.0 score "./backend/kubescore/interactive-in.yaml" --enable-optional-test strings -o ${format} > "./backend/kubescore/interactive-out.json"
