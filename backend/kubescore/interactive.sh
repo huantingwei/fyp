9096628d33c408e805aaeebe2671deba1cadfbbc
huantingwei <ting02940@gmail.com>
2021-05-08 20:37:47 +0800

    fix: docker run --rm

--- backend/kubescore/interactive.sh
+++ backend/kubescore/interactive.sh
@@ -2,4 +2,4 @@
 
 format="json"
 
-sudo docker run -v --rm $(pwd):/project zegl/kube-score:v1.10.0 score "./backend/kubescore/interactive-in.yaml" -o ${format} > "./backend/kubescore/interactive-out.json"
+sudo docker run --rm -v $(pwd):/project zegl/kube-score:v1.10.0 score "./backend/kubescore/interactive-in.yaml" -o ${format} > "./backend/kubescore/interactive-out.json"
